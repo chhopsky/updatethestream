9d3bde64a91c5f0c09e4a20a09d7a7f927d3e192
chhopsky <chris@chhopsky.tv>
2021-11-28 00:09:28 -0800

    UI plumbing, match history

--- tourneydefs.py
+++ tourneydefs.py
@@ -20,13 +20,17 @@ class Match(BaseModel):
 class Game(BaseModel):
     match: int = 0
     winner: int = 3
+    scores: List[int] = [0, 0]
 
 class Tournament(BaseModel):
     def load_from(self, filename="tournament-config.json"):
         with open(filename) as f:
             data = json.load(f)
 
-        self.teams = data["teams"]
+        for tricode, team in data["teams"].items():
+            print(team)
+            team = Team(**team)
+            self.add_edit_team(team)
 
         for current_match in data["matches"]:
             match = Match(**current_match)
@@ -42,16 +46,26 @@ class Tournament(BaseModel):
         for index, match in enumerate(self.matches):
             print(match)
             f_teams = open(f"streamlabels\match-{index}-teams.txt", "w")
-            f_teams.write(f"{self.teams[match.teams[0]]['name']}\n")
-            f_teams.write(f"{self.teams[match.teams[1]]['name']}\n")
+            f_teams.write(f"{self.teams[match.teams[0]].name}\n")
+            f_teams.write(f"{self.teams[match.teams[1]].name}\n")
             f_teams.close()
             f_scores = open(f"streamlabels\match-{index}-scores.txt", "w")
             f_scores.write(f"{match.scores[0]}\n")
             f_scores.write(f"{match.scores[1]}\n")
             f_scores.close()
         return
+    
+    def update_match_scores(self):
+        self.current_match = 0
+        for i in range(len(self.matches)):
+            self.matches[i].scores = [0, 0]
+            self.matches[i].winner = 2
+            self.matches[i].finished = False
+            self.matches[i].in_progress = False
+        for game in self.game_history:
+            self.process_game(game.match, game.winner)
 
-    def game_complete(self, match_id, winner_index):
+    def process_game(self, match_id, winner_index):
         cutoff = self.matches[match_id].best_of / 2
         self.matches[match_id].scores[winner_index] += 1
         self.matches[match_id].in_progress = True
@@ -60,9 +74,15 @@ class Tournament(BaseModel):
             self.matches[match_id].finished = True
             self.matches[match_id].winner = winner_index
             self.current_match += 1
-        
+
+    def game_complete(self, match_id, winner_index):
+        self.process_game(match_id, winner_index)
         finished_game = Game(match = match_id, winner=winner_index)
         self.game_history.append(finished_game)
+
+    def add_edit_team(self, team_to_add_edit, callback = None):
+        self.teams[team_to_add_edit.tricode] = team_to_add_edit
+        print(self.teams)
 
     teams: Dict = {}
     matches: List[Match] = []
--- udts.py
+++ udts.py
@@ -11,6 +11,45 @@ class Ui(QtWidgets.QMainWindow):
 def setup():
     window.team1_win_button.clicked.connect(on_team1win_click)
     window.team2_win_button.clicked.connect(on_team2win_click)
+    window.add_team_button.clicked.connect(add_team)
+    window.refresh_ui_button.clicked.connect(force_refresh_ui)
+    window.refresh_stream_button.clicked.connect(force_refresh_stream)
+    window.undo_button.clicked.connect(undo)
+    window.undo_button.setEnabled(False)
+    populate_teams()
+    populate_matches()
+    refresh_team_win_labels()
+    set_button_states()
+
+def undo():
+    broadcast.game_history.pop()
+    broadcast.update_match_scores()
+    broadcast.write_to_stream()
+    refresh_team_win_labels()
+    set_button_states()
+
+def set_button_states():
+    # rules for when to enable or disable buttons
+    # undo button is disabled if at the first game
+    if len(broadcast.game_history) == 0:
+        window.undo_button.setEnabled(False)
+    else:
+        window.undo_button.setEnabled(True)
+
+    # disable win buttons if tournament is over
+    if broadcast.current_match < len(broadcast.matches):
+        set_team_win_buttons_enabled(True)
+    else:
+        set_team_win_buttons_enabled(False)
+
+def force_refresh_stream():
+    broadcast.write_to_stream()
+
+def force_refresh_ui():
+    populate_teams()
+    populate_matches()
+    refresh_team_win_labels()
+    set_button_states()
     
 def on_team1win_click():
     team_won(0)
@@ -20,15 +59,42 @@ def on_team2win_click():
 
 def team_won(team):
     print(f"team {team} won")
+    match_in_progress = broadcast.current_match
     broadcast.game_complete(broadcast.current_match, team)
     broadcast.write_to_stream()
-    if broadcast.current_match > len(broadcast.matches) - 1:
-        window.team1_win_button.setEnabled(False)
-        window.team2_win_button.setEnabled(False)
-    else:
+    if match_in_progress != broadcast.current_match:
+        set_button_states()
+        refresh_team_win_labels()
+
+def set_team_win_buttons_enabled(new_state = True):
+    window.team1_win_button.setEnabled(new_state)
+    window.team2_win_button.setEnabled(new_state)
+
+def refresh_team_win_labels():
+    if broadcast.current_match < len(broadcast.matches):
         window.team1_win_button.setText(broadcast.matches[broadcast.current_match].teams[0])
         window.team2_win_button.setText(broadcast.matches[broadcast.current_match].teams[1])
 
+def add_team():
+    new_team = Team()
+    new_team.name = window.add_team_name_field.text()
+    new_team.tricode = window.add_team_tricode_field.text()
+    broadcast.add_edit_team(new_team)
+    window.add_team_name_field.setText("")
+    window.add_team_tricode_field.setText("")
+    populate_teams()
+
+def populate_teams():
+    window.team_list_widget.clear()
+    for tricode, team in broadcast.teams.items():
+        item = QtWidgets.QListWidgetItem(f"{team.tricode} {team.name}")
+        window.team_list_widget.addItem(item)
+
+def populate_matches():
+    window.match_list_widget.clear()
+    for match in broadcast.matches:
+        item = QtWidgets.QListWidgetItem(f"{match.teams[0]} vs {match.teams[1]}, BO{match.best_of}")
+        window.match_list_widget.addItem(item)
 
 broadcast = Tournament()
 broadcast.load_from()
