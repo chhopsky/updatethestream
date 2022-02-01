73f9b55a93a24e00ab05b0b9e0f1d9f75a11e5b6
chhopsky <chris@chhopsky.tv>
2022-01-31 20:53:39 -0800

    Improve tricode determiner

--- processors.py
+++ processors.py
@@ -11,7 +11,7 @@ def consonants_only(teamname):
     return new_teamname
 
 def strip_team_prefix(teamname):
-    return teamname.upper().lstrip("TEAM").lstrip("_").lstrip(" ").lstrip("-")
+    return teamname.lstrip("TEAM").lstrip("_").lstrip(" ").lstrip("-")
 
 def is_tricode_unique(teamname, tricodelist):
     if teamname in tricodelist or len(teamname) < 2 or " " in teamname:
@@ -29,33 +29,34 @@ def generate_random_tricode_from_name(teamname, anything=False):
     i.append(random.choice(range(len(source_string))))
     i.append(random.choice(range(len(source_string))))
     new_tricode = source_string[i[0]] + source_string[i[1]] + source_string[i[2]]
-    return new_tricode.upper()
+    return new_tricode
 
 def determine_tricode(teamname, tricodelist):
+    teamname = teamname.upper()
     if " " in teamname or "_" in teamname or "-" in teamname:
         new_teamname = teamname.replace("_", " ").replace("-"," ")
         teamname_split = new_teamname.split(" ")
         if len(teamname_split) > 1:
             if len(teamname_split) >= 3:
                 if len(teamname_split[0]) and len(teamname_split[1]) and len(teamname_split[2]):
-                    if is_tricode_unique(f"{teamname_split[0][0:1]}{teamname_split[1][0:1]}{teamname_split[2][0:1]}".upper(), tricodelist):
-                        return f"{teamname_split[0][0:1]}{teamname_split[1][0:1]}{teamname_split[2][0:1]}".upper()
+                    if is_tricode_unique(f"{teamname_split[0][0:1]}{teamname_split[1][0:1]}{teamname_split[2][0:1]}", tricodelist):
+                        return f"{teamname_split[0][0:1]}{teamname_split[1][0:1]}{teamname_split[2][0:1]}"
             if len(teamname_split) == 2:
                 if len(teamname_split[0]) and len(teamname_split[1]):
-                    if is_tricode_unique(f"{teamname_split[0][0:1]}{teamname_split[1][0:1]}".upper(), tricodelist):
-                        return f"{teamname_split[0][0:1]}{teamname_split[1][0:1]}".upper()
+                    if is_tricode_unique(f"{teamname_split[0][0:1]}{teamname_split[1][0:1]}", tricodelist):
+                        return f"{teamname_split[0][0:1]}{teamname_split[1][0:1]}"
                     if len(teamname_split[0]) > 1 and len(teamname_split[1]) > 1:
-                        if is_tricode_unique(f"{teamname_split[0][0:2]}{teamname_split[1][0:1]}".upper(), tricodelist) and not teamname_split[0].upper().startswith("TEAM"):
-                            return f"{teamname_split[0][0:2]}{teamname_split[1][0:1]}".upper()
-                        if is_tricode_unique(f"{teamname_split[0][0:1]}{teamname_split[1][0:2]}".upper(), tricodelist) and not teamname_split[0].upper().startswith("TEAM"):
-                            return f"{teamname_split[0][0:1]}{teamname_split[1][0:2]}".upper()
+                        if is_tricode_unique(f"{teamname_split[0][0:2]}{teamname_split[1][0:1]}", tricodelist) and not teamname_split[0].startswith("TEAM"):
+                            return f"{teamname_split[0][0:2]}{teamname_split[1][0:1]}"
+                        if is_tricode_unique(f"{teamname_split[0][0:1]}{teamname_split[1][0:2]}", tricodelist) and not teamname_split[0].startswith("TEAM"):
+                            return f"{teamname_split[0][0:1]}{teamname_split[1][0:2]}"
     if "." in teamname:
         teamname_split = teamname.split(".")
         if len(teamname_split) > 1:
             if len(teamname_split[0]) and len(teamname_split[1]):
                 if is_tricode_unique(f"{teamname_split[0:1]}{teamname_split[1][0:2]}", tricodelist):
                     return f"{teamname_split[0][0:1]}{teamname_split[1][0:2]}"
-    if teamname.upper().startswith("TEAM"):
+    if teamname.startswith("TEAM"):
         new_teamname = strip_team_prefix(teamname)
         if is_tricode_unique(f"T{new_teamname[0:2]}", tricodelist):
             return f"T{new_teamname[0:2]}"
@@ -64,28 +65,28 @@ def determine_tricode(teamname, tricodelist):
         if is_tricode_unique(f"T{new_teamname[0:1]}{new_teamname[-1:]}", tricodelist) and len(new_teamname) > 2:
             return f"T{new_teamname[0:1]}{new_teamname[-1:]}"
     if len(teamname) > 2:
-        attempt = f"T{consonants_only(strip_team_prefix(teamname))[0:2].upper()}"
-        if teamname.upper().startswith("TEAM") and is_tricode_unique(attempt, tricodelist):
+        attempt = f"T{consonants_only(strip_team_prefix(teamname))[0:2]}"
+        if teamname.startswith("TEAM") and is_tricode_unique(attempt, tricodelist):
             if len(attempt) > 1 and is_tricode_unique(attempt, tricodelist):
                 return attempt
     teamname = f"{teamname}".replace(" ", "")
     if " " in teamname[0:3] and len(teamname) > 2:
-        if is_tricode_unique(new_teamname[0:3].upper(), tricodelist):
-            return new_teamname[0:3].upper()
-        attempt = f"{new_teamname[0:1].upper()}{consonants_only(new_teamname[2:4].upper())}"
+        if is_tricode_unique(new_teamname[0:3], tricodelist):
+            return new_teamname[0:3]
+        attempt = f"{new_teamname[0:1]}{consonants_only(new_teamname[2:4])}"
         if is_tricode_unique(attempt, tricodelist) and len(attempt) > 1:
             return attempt
-    if is_tricode_unique(teamname[0:3].upper(), tricodelist):
-        return teamname[0:3].upper()
+    if is_tricode_unique(teamname[0:3], tricodelist):
+        return teamname[0:3]
     if len(teamname) > 2:
-        if is_tricode_unique(consonants_only(teamname)[0:3].upper(), tricodelist):
-            if len(f"{consonants_only(teamname)[0:3].upper()}") > 1:
-                return consonants_only(teamname)[0:3].upper()
+        if is_tricode_unique(consonants_only(teamname)[0:3], tricodelist):
+            if len(f"{consonants_only(teamname)[0:3]}") > 1:
+                return consonants_only(teamname)[0:3]
     if len(teamname) > 3:
-        if is_tricode_unique(f"{teamname[0:0]}{teamname[-1:]}".upper(), tricodelist):
-            return f"{teamname[0:0]}{teamname[-1:].upper()}"
-        if is_tricode_unique(f"{teamname[0:0]}{teamname[-2:]}".upper(), tricodelist):
-            return f"{teamname[0:0]}{teamname[-2:].upper()}"
+        if is_tricode_unique(f"{teamname[0:0]}{teamname[-1:]}", tricodelist):
+            return f"{teamname[0:0]}{teamname[-1:]}"
+        if is_tricode_unique(f"{teamname[0:0]}{teamname[-2:]}", tricodelist):
+            return f"{teamname[0:0]}{teamname[-2:]}"
     
     found_tricode = False
     random_tricode = False
